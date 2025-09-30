a7bdb3710bd88b96ec4b88caefda797847653bba
swathi-m12 <swathi@sedintechnologies.com>
2025-09-30 15:12:34 +0530

    first commit

---
+++ test_calculator.py
@@ -0,0 +1,19 @@
+from calculator import add, subtract, multiply, divide
+import pytest
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+
+def test_subtract():
+    assert subtract(5, 2) == 3
+    assert subtract(0, 4) == -4
+
+def test_multiply():
+    assert multiply(3, 4) == 12
+    assert multiply(-2, 5) == -10
+
+def test_divide():
+    assert divide(10, 2) == 5
+    with pytest.raises(ValueError):   # test exception
+        divide(10, 0)
